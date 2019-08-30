5a489e145a560cc20b1f4aa7d3733594050a2cb9
Riquelme Ciro <ciroriquelme05@gmail.com>
2019-08-29 21:48:22 -0300

    Interfaz Terminada

--- app/src/main/java/com/example/sendmeal/MainActivity.java
+++ app/src/main/java/com/example/sendmeal/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    View layi;
+    Switch sw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        layi = findViewById(R.id.layoutCuenta);
+
+
+
+        sw = (Switch) findViewById(R.id.optEsVendedor);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    layi.setVisibility(View.VISIBLE);
+                }else{
+                    layi.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+
+
+
+
+
     }
 }
